85abcb08592b85d6f91a469dc700a324787d3e86
Pier.W <factory666@live.it>
2019-03-19 16:42:18 +0100

    completed!

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ $(document).ready(init);
 function listGenerator(number) {
 
     $.ajax({
-      url: "https://www.boolean.careers/api/array/basket?n=" + number,
+      url: "https://www.boolean.careers/api/array/basket?n=", // + number,
       method: "GET",
+      data : { n : number },
       success: function(dati, stato) {
 
         console.log(dati);
@@ -21,6 +22,20 @@ function listGenerator(number) {
           var container = $(".left");
               container.append(completeHtml);
         }
+//------------------------------------------------------------------------------
+        //Puoi comprimere il codice passando direttamente quello che il server ti passa
+        // for (var i = 0; i < number; i++) {
+        // 	var completeHtml = template(dati.response[i]);
+        //     $(".left").append(completeHtml);
+        // }
+        //
+        // <script id="entry-template" type="text/x-handlebars-template">
+        //   <div class="mystyle">
+        //     <strong>{{ playerCode }}</strong>
+        //   </div>
+        // </script>
+//------------------------------------------------------------------------------
+        outputData(dati.response);
       },
       error: function(richiesta, stato, errori) {
         alert("Errore di connessione");
@@ -28,10 +43,25 @@ function listGenerator(number) {
     });
 }
 
+function outputData(array) {
+
+  var players = $(".mystyle");
+  players.click(function() {
+
+    var me = $(this);
+    var index = me.index();
+
+    var source = $("#dataplayers").html();
+    var template = Handlebars.compile(source);
+
+    var completeHtml = template(array[index]);
+    $(".player-wrapper").html(completeHtml);
+  });
+}
+
 function init() {
 
   var ask = prompt("Quanti players vuoi?");
       ask = Number(ask);
   listGenerator(ask);
-
 }
